5cc7f59d0e373cc14f80a2c4bb175372402135b2
Filip Jovakaric <filip.jovakaric@gmail.com>
2019-12-20 16:46:25 +0100

    CN-4499: Android header with notch bug (#482)
    * CN-4499: fix heading bug
    
    * CN-4499: code cleanup

--- app/components/organisms/CelHeading/CelHeading.styles.js
+++ app/components/organisms/CelHeading/CelHeading.styles.js
@@ -1,9 +1,12 @@
-import { StatusBar, Platform } from "react-native";
-import DeviceInfo from "react-native-device-info";
+import { Platform } from "react-native";
+// import DeviceInfo from "react-native-device-info";
 import { getThemedStyle } from "../../../utils/styles-util";
 import STYLES from "../../../constants/STYLES";
 
-const hasNotch = DeviceInfo.hasNotch();
+// const hasNotch = DeviceInfo.hasNotch();
+// const isAndroidWithNotch = Platform.OS === "android" && hasNotch
+// const paddingTop = Platform.OS === "android" && !isAndroidWithNotch ? StatusBar.currentHeight : 0
+const headerHeight = 60;
 
 const base = {
   content: {
@@ -11,28 +14,32 @@ const base = {
     justifyContent: "center",
     alignItems: "center",
     flexDirection: "row",
-    height: Platform.OS === "android" && hasNotch ? 30 : 60,
+    height: headerHeight,
   },
 
   center: {
     alignSelf: "center",
     justifyContent: "center",
+    flex: 1,
   },
 
   left: {
-    flex: 1,
     alignItems: "flex-start",
+    justifyContent: "center",
+    top: 0,
+    minWidth: "15%",
+    height: headerHeight,
   },
 
   right: {
-    flex: 1,
     alignItems: "flex-end",
-    top: Platform.OS === "android" && hasNotch ? -30 : 0,
+    justifyContent: "center",
+    top: 0,
+    minWidth: "15%",
+    height: headerHeight,
   },
 
   headingBackground: {
-    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
-
     ...Platform.select({
       android: {
         borderColor: "#E9E9E9",
@@ -45,17 +52,12 @@ const base = {
   },
 
   transparentBackground: {
-    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
     position: "absolute",
     backgroundColor: "transparent",
     zIndex: 100,
     top: 0,
     left: 0,
     right: 0,
-  },
-
-  sameBackground: {
-    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
   },
 
   profilePicture: {
