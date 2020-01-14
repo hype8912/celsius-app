069f9a8e7add646861a482247aa94ec5a747da94
Ivan Crnogorac <crrnii@yahoo.com>
2020-01-14 10:32:24 +0100

    CN-3723: ReCaptcha updated

--- app/utils/recaptcha-util.js
+++ app/utils/recaptcha-util.js
@@ -1,5 +1,8 @@
-import React from "react";
+import React, {Component} from "react";
 import { WebView } from "react-native-webview";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import * as appActions from "../redux/actions";
 
 // fix https://github.com/facebook/react-native/issues/10865
 const patchPostMessageJsCode = `(${String(function() {
@@ -18,87 +21,124 @@ const patchPostMessageJsCode = `(${String(function() {
 
 /**
  *
- * @param {*} onMessage: callback after received response, error of Google captcha or when user cancel
+ * @param {*} onMsg: callback after received response, error of Google captcha or when user cancel
  * @param {*} siteKey: your site key of Google captcha
  * @param {*} style: custom style
  * @param {*} url: base url
- * @param {*} languageCode: can be found at https://developers.google.com/recaptcha/docs/language
- * @param {*} cancelButtonText: title of cancel button
  */
-const GoogleReCaptcha = ({
-                           onMessage,
-                           siteKey,
-                           style,
-                           url,
-                           languageCode,
-                         }) => {
-  const generateTheWebViewContent = key => {
-    const originalForm = `<!DOCTYPE html>
-			<html>
-			<head>
-				<meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1.0">
-				<meta http-equiv="X-UA-Compatible" content="ie=edge">
-				<script src="https://recaptcha.google.com/recaptcha/api.js?explicit&hl=${languageCode ||
-    "en"}"></script>
-				<script type="text/javascript">
-				var onloadCallback = function() { };
-				var onDataCallback = function(response) {
-					window.ReactNativeWebView.postMessage(response);
-					setTimeout(function () {
-						document.getElementById('captcha').style.display = 'none';
-					}, 1500);
-				};
-				var onCancel = function() {
-					window.ReactNativeWebView.postMessage("cancel");
-					document.getElementById('captcha').style.display = 'none';
-				}
-				var onDataExpiredCallback = function(error) {  window.ReactNativeWebView.postMessage("expired"); };
-				var onDataErrorCallback = function(error) {  window.ReactNativeWebView.postMessage("error"); }
-				</script>
-				<style>
-					.btn {
-						background-color: #c60710;
-						color: #ffffff; padding: 8px 32px; margin-top: 8px;
-						border: none; border-radius: 25px; font-weight: bold;
-					}
-					.btn:active {
-						outline: none;
-					}
-					.btn:focus {
-						outline: none;
-					}
-				</style>
-			</head>
-			<body>
-				<div id="captcha">
-					<div style="text-align: center; padding-top: 100px;">
-					<div class="g-recaptcha" style="display: inline-block; height: auto;"
-						data-sitekey="${key}" data-callback="onDataCallback"
-						data-expired-callback="onDataExpiredCallback"
-						data-error-callback="onDataErrorCallback">
-					</div>
 
-					</div>
-				</div>
-			</body>
-			</html>`;
+@connect(
+  state => ({
+    formData: state.forms.formData,
+  }),
+  dispatch => ({ actions: bindActionCreators(appActions, dispatch) })
+)
+
+class GoogleReCaptcha extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      captchaHeight:150
+    }
+  }
+
+  generateTheWebViewContent = key => {
+    const originalForm =
+      `<!DOCTYPE html>
+       <html>
+        <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <script>
+          function onSubmit(token) {
+            // after receiving token send it to message
+            window.ReactNativeWebView.postMessage(token);
+          }
+
+          function validate(event) {
+            event.preventDefault();
+            window.ReactNativeWebView.postMessage('increaseReCap')
+            grecaptcha.execute()
+          }
+
+          function onload() {
+            const element = document.getElementById('submit');
+            element.onclick = validate;
+          }
+        </script>
+        <script src="https://www.google.com/recaptcha/api.js" async defer></script>
+
+        <style>
+          .btn {
+            background-color: #3F51AB;
+            width:110px;
+            height: 50px;
+            color: #ffffff; padding: 8px 32px; margin-top: 8px;
+            border: none; border-radius: 25px; font-size: medium;
+          }
+            .grecaptcha-badge {
+              top: 80px !important;
+              margin-right: 9.2%;
+              transform: scale(0.5);
+            /*visibility: hidden;*/
+            }
+          </style>
+
+        </head>
+        <body>
+            <div
+                id='recaptcha'
+                class="g-recaptcha"
+                data-sitekey="${key}"
+                data-callback="onSubmit"
+                data-size="invisible"
+              >
+            </div>
+            <div style="text-align: center">
+                <button class='btn' id='submit'>Log in</button>
+            </div>
+          <script>onload();</script>
+        </body>
+      </html>
+     `;
+
     return originalForm;
-  };
-  return (
-    <WebView
-      originWhitelist={["*"]}
-      mixedContentMode={"always"}
-      onMessage={onMessage}
-      javaScriptEnabled
-      injectedJavaScript={patchPostMessageJsCode}
-      automaticallyAdjustContentInsets
-      style={[{ backgroundColor: "transparent", width: "100%" }, style]}
-      source={{
-        html: generateTheWebViewContent(siteKey),
-        baseUrl: `${url}`
-      }}
-    />
-  );
-};
+  }
 
-export default GoogleReCaptcha;
+  onMsg = event => {
+    const { formData, reCaptchaPassed } = this.props
+
+    if (event && event.nativeEvent.data && event.nativeEvent.data === 'increaseReCap') {
+      if (formData.reCaptchaKey) { reCaptchaPassed(event) }
+      this.setState({
+        captchaHeight:500
+      })
+    }
+    else {
+      reCaptchaPassed(event)
+      this.setState({captchaHeight: 150})
+    }
+  };
+
+
+  render(){
+    const { siteKey, style, url } = this.props
+    const { captchaHeight } = this.state
+    return(
+      <WebView
+        originWhitelist={["*"]}
+        mixedContentMode={"always"}
+        onMessage={this.onMsg}
+        javaScriptEnabled
+        injectedJavaScript={patchPostMessageJsCode}
+        automaticallyAdjustContentInsets
+        style={[{ backgroundColor: "transparent", width: "100%" , height: captchaHeight}, style]}
+        source={{
+          html: this.generateTheWebViewContent(siteKey),
+          baseUrl: `${url}`
+        }}
+      />
+    )
+  }
+
+} export default GoogleReCaptcha
