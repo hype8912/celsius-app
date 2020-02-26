3e0ed70351912664279d2eff5678e5ffb71fea61
fj <filip.jovakaric@gmail.com>
2020-02-26 18:12:19 +0100

    CN-4836: enabling manual code push of the app

--- app/utils/branch-util.js
+++ app/utils/branch-util.js
@@ -1,4 +1,5 @@
 import Branch from "react-native-branch";
+import codePush from "react-native-code-push";
 
 import store from "../redux/store";
 import * as actions from "../redux/actions";
@@ -24,8 +25,17 @@ function initBranch() {
           !deepLink.params["+clicked_branch_link"] ||
           deepLink.error ||
           !deepLink.params
-        )
+        ) {
+          // Fetch CodePush Update and Restart App
+          // if no link provided and update is available
+          codePush.sync({
+            updateDialog: false,
+            installMode: codePush.InstallMode.IMMEDIATE,
+          });
+
           return;
+        }
+
         dispatch(actions.registerBranchLink(deepLink));
       });
     } catch (error) {
