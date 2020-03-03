de25cd5effe984587a1b643f2796b34c36fe2b36
Ivan Crnogorac <crrnii@yahoo.com>
2020-03-03 10:12:11 +0100

    CN-4889: Refactor geting image

--- app/components/atoms/SecurityStrengthMeter/SecurityStrengthMeter.js
+++ app/components/atoms/SecurityStrengthMeter/SecurityStrengthMeter.js
@@ -1,24 +1,15 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
-import { connect } from "react-redux";
-import { bindActionCreators } from "redux";
 import { Image, View } from "react-native";
 
 import STYLES from "../../../constants/STYLES";
 import CelText from "../../atoms/CelText/CelText";
-import * as appActions from "../../../redux/actions";
 import { getTheme } from "../../../utils/styles-util";
 import SecurityStrengthMeterStyle from "./SecurityStrengthMeter.styles";
 import Card from "../Card/Card";
 // import StoryWrapper from "../../atoms/StoryWrapper/StoryWrapper";
 
-@connect(
-  state => ({
-    interestRates: state.generalData.interestRates,
-  }),
-  dispatch => ({ actions: bindActionCreators(appActions, dispatch) })
-)
-class SwitchScoreGauge extends Component {
+class SecurityStrengthMeter extends Component {
   static propTypes = {
     subtitle: PropTypes.string,
     onPress: PropTypes.func,
@@ -26,41 +17,49 @@ class SwitchScoreGauge extends Component {
   };
   static defaultProps = {};
 
+  getImage = description => {
+    const theme = getTheme();
+    const images = {
+      lightWeak: require("../../../../assets/images/security-meter/01-pass-strength-weak-light.png"),
+      darkWeak: require("../../../../assets/images/security-meter/01-pass-strength-weak-dark.png"),
+
+      lightFair: require("../../../../assets/images/security-meter/01-pass-strength-weak-light.png"),
+      darkFair: require("../../../../assets/images/security-meter/01-pass-strength-weak-dark.png"),
+
+      lightGood: require("../../../../assets/images/security-meter/03-pass-strength-good-light.png"),
+      darkGood: require("../../../../assets/images/security-meter/03-pass-strength-good-dark.png"),
+
+      lightStrong: require("../../../../assets/images/security-meter/04-pass-strength-strong.png"),
+      darkStrong: require("../../../../assets/images/security-meter/04-pass-strength-strong.png"),
+    };
+    return images[`${theme}${description}`];
+  };
+
   getMeterProps = () => {
     const { level } = this.props;
-    const theme = getTheme();
     switch (level) {
       case "1":
         return {
           text: "WEAK",
-          imageUrl:
-            theme === "light"
-              ? require("../../../../assets/images/security-meter/01-pass-strength-weak-light.png")
-              : require("../../../../assets/images/security-meter/01-pass-strength-weak-dark.png"),
+          imageUrl: this.getImage("Weak"),
           textColor: STYLES.COLORS.RED,
         };
       case "2":
         return {
           text: "FAIR",
-          imageUrl:
-            theme === "light"
-              ? require("../../../../assets/images/security-meter/02-pass-strength-fair-light.png")
-              : require("../../../../assets/images/security-meter/02-pass-strength-fair-dark.png"),
+          imageUrl: this.getImage("Fair"),
           textColor: STYLES.COLORS.ORANGE_DARK,
         };
       case "3":
         return {
           text: "GOOD",
-          imageUrl:
-            theme === "light"
-              ? require("../../../../assets/images/security-meter/03-pass-strength-good-light.png")
-              : require("../../../../assets/images/security-meter/03-pass-strength-good-dark.png"),
+          imageUrl: this.getImage("Good"),
           textColor: STYLES.COLORS.ORANGE,
         };
       case "4":
         return {
           text: "STRONG",
-          imageUrl: require("../../../../assets/images/security-meter/04-pass-strength-strong.png"),
+          imageUrl: this.getImage("Strong"),
           textColor: STYLES.COLORS.GREEN,
         };
       default:
@@ -97,4 +96,4 @@ class SwitchScoreGauge extends Component {
   }
 }
 
-export default SwitchScoreGauge;
+export default SecurityStrengthMeter;
