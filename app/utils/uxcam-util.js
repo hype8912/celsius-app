8fd98e91d7d9e6946976093cf3f6929bfb33c0b5
Nikola Sabelnik <sabelnikola@gmail.com>
2020-04-02 15:33:10 +0200

    CN-4839 adding uxcam api key constant

--- app/utils/uxcam-util.js
+++ app/utils/uxcam-util.js
@@ -1,5 +1,8 @@
 import RNUxcam from "react-native-ux-cam";
 import loggerUtil from "./logger-util";
+import Constants from "../../constants";
+
+const { UXCAM_API_KEY } = Constants;
 
 export{
     initUxCam
@@ -7,9 +10,11 @@ export{
 
 async function initUxCam() {
 
+  if (!UXCAM_API_KEY) return;
+
   try {
     await RNUxcam.optIntoSchematicRecordings(); // for enabling iOS screen recordings
-    await RNUxcam.startWithKey("9hfcebj8jl88oba"); // Add this line after RNUcam.optIntoSchematicRecordings();
+    await RNUxcam.startWithKey(UXCAM_API_KEY); // Add this line after RNUcam.optIntoSchematicRecordings();
   } catch(err) {
     loggerUtil.log(err)
   }
