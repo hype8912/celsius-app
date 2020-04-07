e7d45d8d63bb105cef723b2859e58edbec54716c
Nikola <30319637+ShamanShaulic@users.noreply.github.com>
2020-04-07 08:48:01 +0200

    CN-5051 Add info cards to transactions (#652)

--- app/components/screens/TransactionDetailsWithdraw/TransactionDetailsWithdraw.js
+++ app/components/screens/TransactionDetailsWithdraw/TransactionDetailsWithdraw.js
@@ -3,6 +3,7 @@ import { View } from "react-native";
 import PropTypes from "prop-types";
 import moment from "moment";
 
+import TransactionWithdrawDetailsStyle from "./TransactionDetailsWithdraw.styles"
 import RegularLayout from "../../layouts/RegularLayout/RegularLayout";
 import TxInfoSection from "../../atoms/TxInfoSection/TxInfoSection";
 import TxAddressSection from "../../atoms/TxAddressSection/TxAddressSection";
@@ -10,6 +11,9 @@ import TxBasicSection from "../../atoms/TxBasicSection/TxBasicSection";
 import CelButton from "../../atoms/CelButton/CelButton";
 import STYLES from "../../../constants/STYLES";
 import { TRANSACTION_TYPES } from "../../../constants/DATA";
+import InfoBox from "../../atoms/InfoBox/InfoBox";
+import CelText from "../../atoms/CelText/CelText";
+import Icon from "../../atoms/Icon/Icon";
 
 class TransactionDetailsWithdraw extends Component {
   static propTypes = {
@@ -21,6 +25,7 @@ class TransactionDetailsWithdraw extends Component {
     // const style = TransactionDetailsDepositsStyle();
     const { transaction, actions } = this.props;
     const transactionProps = transaction.uiProps;
+    const style = TransactionWithdrawDetailsStyle()
 
     return (
       <RegularLayout>
@@ -30,6 +35,48 @@ class TransactionDetailsWithdraw extends Component {
             transaction={transaction}
             transactionProps={transactionProps}
           />
+
+          {transaction.type === TRANSACTION_TYPES.WITHDRAWAL_PENDING_REVIEW && (
+            <InfoBox
+              backgroundColor={STYLES.COLORS.CELSIUS_BLUE}
+              padding={"20 30 20 10"}
+            >
+              <View style={style.direction}>
+                <Icon
+                  name={"Info"}
+                  height="25"
+                  width="25"
+                  fill={STYLES.COLORS.WHITE}
+                />
+                <CelText color={"white"} margin={"0 10 0 10"}>
+                  Due to the larger amount of funds being sent, this transaction may take a little bit longer.
+                </CelText>
+              </View>
+
+              </InfoBox>
+          )}
+
+          {transaction.type === TRANSACTION_TYPES.WITHDRAWAL_PENDING_VERIFICATION && (
+            <InfoBox
+              backgroundColor={STYLES.COLORS.ORANGE}
+              padding={"20 20 20 10"}
+            >
+              <View style={style.direction}>
+                <View style={style.circle}>
+              <Icon
+                name={"Mail"}
+                height="15"
+                width="15"
+                fill={STYLES.COLORS.RED}
+              />
+                </View>
+              <CelText color={"white"} margin={"0 10 0 10"}>
+                In order to proceed, you must confirm the transaction via email.
+              </CelText>
+              </View>
+            </InfoBox>
+          )}
+
           <TxAddressSection
             transaction={transaction}
             address={transaction.from_address}
