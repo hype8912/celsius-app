7584991f4968e40572905eeebeb35bfdc2a72f0d
Nikola Sabelnik <sabelnikola@gmail.com>
2020-05-04 18:35:52 +0200

    CN-5529 Added Margin Call Modal

---
+++ app/components/modals/MarginCallModal/MarginCallModal.styles.js
@@ -0,0 +1,37 @@
+// import STYLES from '../../../constants/STYLES';
+import { getThemedStyle } from "../../../utils/styles-util";
+import STYLES from "../../../constants/STYLES";
+
+const base = {
+  container: {
+    flex: 1,
+  },
+  buttonsWrapper: {
+    height: 50,
+  },
+  wrapper: {
+    marginHorizontal: 20,
+  },
+  loanToValue: {
+    marginVertical: 10,
+  },
+};
+
+const themed = {
+  light: {
+    background: {
+      backgroundColor: STYLES.COLORS.LIGHT_GRAY,
+    },
+  },
+
+  dark: {
+    background: {
+      backgroundColor: STYLES.COLORS.DARK_SECONDARY_BUTTON_GRAY,
+    },
+  },
+  celsius: {},
+};
+
+const MarginCallModalStyle = () => getThemedStyle(base, themed);
+
+export default MarginCallModalStyle;
