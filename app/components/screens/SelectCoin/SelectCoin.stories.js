fa360235e17ec181f4a358eb5e38ab65f7beda2c
fj <filip.jovakaric@gmail.com>
2020-07-01 12:18:15 +0200

    CN-6386: adding SelectCoin stories

---
+++ app/components/screens/SelectCoin/SelectCoin.stories.js
@@ -0,0 +1,44 @@
+import React from "react";
+
+import SelectCoin from "./SelectCoin";
+import ScreenStoryWrapper from "../../../../storybook/stories/ScreenStoryWrapper/ScreenStoryWrapper";
+import mockCurrenciesStore from "../../../../celsius-app-creds/mock-data/mockCurrenciesStore";
+import mockComplianceStore from "../../../../celsius-app-creds/mock-data/mockComplianceStore";
+import { SIMPLEX_FIAT_CURRENCIES } from "../../../constants/DATA";
+
+const initialState = {
+  currencies: {
+    rates: mockCurrenciesStore.rates,
+  },
+  compliance: mockComplianceStore.allowedAll,
+};
+
+const navigationProps = {
+  coinListFormatted: mockCurrenciesStore.rates.map(c => ({
+    label: c.name,
+    value: c.short,
+  })),
+};
+
+const crypto = () => (
+  <ScreenStoryWrapper
+    screen={SelectCoin}
+    screenName="SelectCoin"
+    state={initialState}
+    navigationProps={navigationProps}
+  />
+);
+
+const fiat = () => (
+  <ScreenStoryWrapper
+    screen={SelectCoin}
+    screenName="SelectCoin"
+    state={initialState}
+    navigationProps={{ coinListFormatted: SIMPLEX_FIAT_CURRENCIES }}
+  />
+);
+
+export default {
+  crypto,
+  fiat,
+};
