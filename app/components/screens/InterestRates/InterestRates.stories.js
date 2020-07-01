4571b5b0bc09259d3e8edfc180b14d0c33373077
fj <filip.jovakaric@gmail.com>
2020-07-01 12:16:32 +0200

    CN-6386: adding InterestRates stories

---
+++ app/components/screens/InterestRates/InterestRates.stories.js
@@ -0,0 +1,39 @@
+import React from "react";
+
+import InterestRates from "./InterestRates";
+import ScreenStoryWrapper from "../../../../storybook/stories/ScreenStoryWrapper/ScreenStoryWrapper";
+import mockUserStore from "../../../../celsius-app-creds/mock-data/mockUserStore";
+import mockLoyaltyStore from "../../../../celsius-app-creds/mock-data/mockLoyaltyStore";
+import mockGeneralDataStore from "../../../../celsius-app-creds/mock-data/mockGeneralDataStore";
+import mockWalletStore from "../../../../celsius-app-creds/mock-data/mockWalletStore";
+import mockCurrenciesStore from "../../../../celsius-app-creds/mock-data/mockCurrenciesStore";
+import mockComplianceStore from "../../../../celsius-app-creds/mock-data/mockComplianceStore";
+
+const initialState = {
+  user: {
+    profile: mockUserStore.profile.postman13,
+    appSettings: mockUserStore.appSettings.postman13,
+  },
+  wallet: {
+    summary: mockWalletStore.summary.postman13,
+  },
+  currencies: {
+    rates: mockCurrenciesStore.rates,
+    currencyRatesShort: mockCurrenciesStore.currencyRatesShort,
+  },
+  compliance: mockComplianceStore.allowedAll,
+  loyalty: mockLoyaltyStore.loyalty.postman13,
+  generalData: mockGeneralDataStore,
+};
+
+const regular = () => (
+  <ScreenStoryWrapper
+    screen={InterestRates}
+    screenName="InterestRates"
+    state={initialState}
+  />
+);
+
+export default {
+  regular,
+};
