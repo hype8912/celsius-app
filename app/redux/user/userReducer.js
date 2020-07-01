264a5a94f8d6617606483fe22ff87d53faff9409
Djuro Alfirevic <djalfirevic@gmail.com>
2020-07-01 16:32:44 +0200

    CN-6274: Set initial theme

--- app/redux/user/userReducer.js
+++ app/redux/user/userReducer.js
@@ -1,5 +1,6 @@
 import ACTIONS from "../../constants/ACTIONS";
 import { mapProfile } from "../../utils/user-util";
+import { THEMES } from "../../constants/UI";
 
 /**
  * TODO make it a function add JSDoc & desc for return
@@ -11,7 +12,7 @@ const initialState = {
 
   // TODO move to userDataReducer
   appSettings: {
-    theme: "light",
+    theme: THEMES.LIGHT,
     activeHodlMode: undefined,
   },
 
