3dacb8251cfffe821225d424e837b066352f518b
fj <filip.jovakaric@gmail.com>
2020-07-15 17:01:57 +0200

    CN-6623: polishing RegisterInitial screen

--- app/components/atoms/PassStrengthMeter/PassStrengthMeter.js
+++ app/components/atoms/PassStrengthMeter/PassStrengthMeter.js
@@ -5,8 +5,9 @@ import { View } from "react-native";
 import PassStrengthMeterStyle from "./PassStrengthMeter.styles";
 import CelText from "../CelText/CelText";
 import securityUtil from "../../../utils/security-util";
-import STYLES from "../../../constants/STYLES";
 import { SECURITY_STRENGTH_LEVEL } from "../../../constants/DATA";
+import { getColor } from "../../../utils/styles-util";
+import { COLOR_KEYS } from "../../../constants/COLORS";
 
 class PassStrengthMeter extends Component {
   static propTypes = {
@@ -22,22 +23,22 @@ class PassStrengthMeter extends Component {
       case SECURITY_STRENGTH_LEVEL.WEAK:
         return {
           level: SECURITY_STRENGTH_LEVEL.WEAK,
-          color: STYLES.COLORS.RED,
+          color: getColor(COLOR_KEYS.NEGATIVE_STATE),
         };
       case SECURITY_STRENGTH_LEVEL.FAIR:
         return {
           level: SECURITY_STRENGTH_LEVEL.FAIR,
-          color: STYLES.COLORS.ORANGE,
+          color: getColor(COLOR_KEYS.ALERT_STATE),
         };
       case SECURITY_STRENGTH_LEVEL.GOOD:
         return {
           level: SECURITY_STRENGTH_LEVEL.GOOD,
-          color: STYLES.COLORS.ORANGE,
+          color: getColor(COLOR_KEYS.ALERT_STATE),
         };
       case SECURITY_STRENGTH_LEVEL.STRONG:
         return {
           level: SECURITY_STRENGTH_LEVEL.STRONG,
-          color: STYLES.COLORS.GREEN,
+          color: getColor(COLOR_KEYS.POSITIVE_STATE),
         };
     }
   };
@@ -68,11 +69,7 @@ class PassStrengthMeter extends Component {
           ]}
         />
         <View style={{ flexDirection: "row" }}>
-          <CelText
-            type={"H6"}
-            color={STYLES.COLORS.MEDIUM_GRAY}
-            margin={"5 0 0 0"}
-          >
+          <CelText type={"H6"} margin={"5 0 0 0"}>
             Password Strength:{" "}
           </CelText>
           <CelText type={"H6"} margin={"5 0 0 0"} color={passStatus.color}>
