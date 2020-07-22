1e228cc4b9f8086a2015ca7ed7a3ed3494903ca6
Srdjan Djenader <djenka76@gmail.com>
2020-07-22 15:44:42 +0200

    CN-6728 reskin multistep modal (#882)
    * CN-6726: enabling modals storybook
    
    * CN-6726: adding button for theme changing
    
    * CN-6729: polishing ApiKeySuccessModal
    
    * CN-6729: polishing loyalty level modal
    
    * CN-6729: removing CreateNewAccountModal
    
    * CN-6729: polishing ConfirmWithdrawalAddressModal
    
    * CN-6726: refactoring DestinationInfoTagModal
    
    * CN-6729: refactoring MemoIdModal
    
    * CN-6729: polishing PrepaymentSuccessfulModal
    
    * CN-6729: polishing ReferralSendModal
    
    * CN-6729: removing helper code
    
    * CN-6729: polishing TransactionsFilterModal
    
    * CN-6729: polishing WithdrawWarningModal
    
    * CN-6727: re-skinned CelModalButton.js and CelModal.styles.js (#881)
    
    Co-authored-by: Srdjan Djenader <djenka76@gmail.com>
    
    * CN-6729: removing unused modal
    
    * CN-6728: re-skinned MultistepModal.js
    
    * CN-6728: removed unused MultiStep
    
    * CN-6728: fixed icons in sunset theme
    
    * CN-6728: minor refactor
    
    * CN-6728: removed unused story, removed mock
    
    Co-authored-by: fj <filip.jovakaric@gmail.com>

--- app/components/modals/LoanApplicationSuccessModal/LoanApplicationSuccessModal.js
+++ app/components/modals/LoanApplicationSuccessModal/LoanApplicationSuccessModal.js
@@ -2,17 +2,10 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
-import { View } from "react-native";
 
-import { LOAN_PAYMENT_REASONS, MODALS, THEMES } from "../../../constants/UI";
+import { LOAN_PAYMENT_REASONS, MODALS } from "../../../constants/UI";
 import InfoModal from "../InfoModalNew/InfoModal";
 import * as appActions from "../../../redux/actions";
-import MultistepModal from "../MultistepModal/MultistepModal";
-import LoanApplicationSuccessModalStyle from "./LoanApplicationSuccessModal.styles";
-import CelModalButton from "../../atoms/CelModalButton/CelModalButton";
-import CelText from "../../atoms/CelText/CelText";
-import multiStepUtil from "../../../utils/multistep-modal-util";
-import { getTheme } from "../../../utils/styles-util";
 
 @connect(
   state => ({
@@ -39,80 +32,7 @@ class LoanApplicationSuccessModal extends Component {
 
   render() {
     const { actions } = this.props;
-    const style = LoanApplicationSuccessModalStyle();
 
-    const theme = getTheme();
-
-    // NOTE: (srdjan) commented after client asked to kick automatic approve loan feature
-
-    // if (
-    //   Number(formData.loanAmount) < Number(automaticLoanLimit) &&
-    //   formData.coin !== "USD"
-    // )
-    if (false) {
-      return (
-        <MultistepModal
-          style={style.container}
-          name={MODALS.LOAN_APPLICATION_SUCCESS_MODAL}
-          top={30}
-          imagesArray={[
-            require("../../../../assets/images/checkmark.png"),
-            theme === THEMES.LIGHT
-              ? require("../../../../assets/images/coin-stack-icon.png")
-              : require("../../../../assets/images/coin-stack-icon-dark.png"),
-          ]}
-          imageHeight={25}
-          imageWidth={25}
-        >
-          <View style={style.modalWrapper}>
-            <CelText
-              type={"H3"}
-              align={"center"}
-              margin={"0 20 5 20"}
-              weight={"700"}
-            >
-              Loan Successfully Initiated
-            </CelText>
-            <CelText align={"center"} margin={"5 20 0 20"}>
-              Thank you for initiating your loan with Celsius. Once approved,
-              your funds will be transferred to your Celsius wallet.
-            </CelText>
-            <View style={style.buttonsWrapper}>
-              <CelModalButton
-                buttonStyle={"secondary"}
-                position={"single"}
-                onPress={() => multiStepUtil.goToNextStep()}
-              >
-                Next
-              </CelModalButton>
-            </View>
-          </View>
-          <View style={style.modalWrapper}>
-            <CelText
-              type={"H3"}
-              align={"center"}
-              margin={"0 20 5 20"}
-              weight={"700"}
-            >
-              Prepay Your Interest
-            </CelText>
-            <CelText align={"center"} margin={"5 20 0 20"}>
-              Your first interest payment will be due next month. Stay ahead of
-              schedule and submit your first interest payment in advance.
-            </CelText>
-            <View style={style.buttonsWrapper}>
-              <CelModalButton
-                buttonStyle={"basic"}
-                position={"single"}
-                onPress={this.handlePrepayModal}
-              >
-                Make a Payment
-              </CelModalButton>
-            </View>
-          </View>
-        </MultistepModal>
-      );
-    }
     return (
       <InfoModal
         name={MODALS.LOAN_APPLICATION_SUCCESS_MODAL}
