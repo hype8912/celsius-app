1b452cc5042c3b38b5d57ecf77923070680efb38
Djuro Alfirevic <djalfirevic@gmail.com>
2020-07-24 12:56:07 +0200

    Removing icon

--- app/components/atoms/CheckEmailInfoBox/CheckEmailInfoBox.js
+++ app/components/atoms/CheckEmailInfoBox/CheckEmailInfoBox.js
@@ -2,7 +2,6 @@ import React from "react";
 import { View } from "react-native";
 
 import CheckEmailInfoBoxStyle from "./CheckEmailInfoBox.styles";
-import Icon from "../Icon/Icon";
 import CelText from "../CelText/CelText";
 import InfoBox from "../InfoBox/InfoBox";
 import { COLOR_KEYS } from "../../../constants/COLORS";
@@ -18,14 +17,6 @@ const CheckEmailInfoBox = props => {
         padding={"20 30 20 10"}
       >
         <View style={style.direction}>
-          <View style={style.circle}>
-            <Icon
-              name={"Mail"}
-              height="20"
-              width="20"
-              fill={COLOR_KEYS.ALERT_STATE}
-            />
-          </View>
           <CelText color={"white"} margin={"0 20 0 10"}>
             {props.infoText}
           </CelText>
