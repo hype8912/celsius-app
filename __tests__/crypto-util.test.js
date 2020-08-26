0674fbd479a0b86cb4538dc569de45013babe44f
Djuro Alfirevic <djalfirevic@gmail.com>
2020-08-26 13:07:36 +0200

    CN-7023: Add --verbose for tests

--- __tests__/crypto-util.test.js
+++ __tests__/crypto-util.test.js
@@ -6,3 +6,20 @@ describe("Crypto Utils - providing link for currency", () => {
     expect(result).toBe("https://buy.moonpay.io/celsius");
   });
 });
+
+describe("Crypto Utils - isGreaterThan", () => {
+  it("should be false", () => {
+    const result = cryptoUtil.isGreaterThan("1.0", "2.0");
+    expect(result).toBe(false);
+  });
+
+  it("should be equal", () => {
+    const result = cryptoUtil.isGreaterThan("2.0", "2.0");
+    expect(result).toBe(false);
+  });
+
+  it("should be greater", () => {
+    const result = cryptoUtil.isGreaterThan("3.0", "2.0");
+    expect(result).toBe(true);
+  });
+});
