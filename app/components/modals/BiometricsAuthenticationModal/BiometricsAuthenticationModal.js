8f355abcf5174df8800bc4678634b69ed2c00ad5
Ivan Crnogorac <crrnii@yahoo.com>
2020-09-03 09:56:17 +0200

    CN-7295: BiometricsNotRecognized created.

--- app/components/modals/BiometricsAuthenticationModal/BiometricsAuthenticationModal.js
+++ app/components/modals/BiometricsAuthenticationModal/BiometricsAuthenticationModal.js
@@ -7,7 +7,6 @@ import { MODALS } from "../../../constants/UI";
 class BiometricsAuthenticationModal extends React.Component {
   goNext = () => {
     const { actions } = this.props;
-    actions.resetToScreen("BiometricAuthentication");
     actions.closeModal();
   };
 
@@ -17,9 +16,10 @@ class BiometricsAuthenticationModal extends React.Component {
         name={MODALS.BIOMETRICS_AUTHENTICATION_MODAL}
         heading={"Biometrics Authentication"}
         paragraphs={[
-          "We have detected that you haven't used biometric on this device so far. Would you like to set it now?",
+          "We have detected that you haven't used biometric on this device so far.",
+          "If you want to set your Biometrics on this device please go to Biometrics authentication settings screen under Security settings",
         ]}
-        yesCopy={"Set Biometrics"}
+        yesCopy={"I understand"}
         onYes={this.goNext}
         picture={require("../../../../assets/images/alert-icon.png")}
         darkPicture={require("../../../../assets/images/alert-icon.png")}
