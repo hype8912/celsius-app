49c0544c6a2cee2bb265f360c52af01ea291a387
Ivan Crnogorac <crrnii@yahoo.com>
2020-09-04 11:49:09 +0200

    Merge branch 'develop' into feature/CN-7098-Biometrics_implementation
    # Conflicts:
    #	app/constants/UI.js
    #	app/utils/ui-util.js

---
+++ app/components/modals/PickerModal/PickerModal.styles.js
@@ -0,0 +1,22 @@
+import { Dimensions } from "react-native";
+import { getThemedStyle } from "../../../utils/styles-util";
+
+const { height } = Dimensions.get("window");
+
+const base = {
+  wrapper: { maxHeight: 0.7 * height, paddingVertical: 20 },
+  scroll: { paddingHorizontal: 40 },
+  item: { height: 40, justifyContent: "center" },
+};
+
+const themed = {
+  light: {},
+
+  dark: {},
+
+  unicorn: {},
+};
+
+const PickerModalStyle = () => getThemedStyle(base, themed);
+
+export default PickerModalStyle;
