1304be535076fa590c4775e67a674894894e99a1
Srdjan Djenader <djenka76@gmail.com>
2020-09-04 10:49:30 +0200

    CN-7397: renamed new theme

--- app/components/screens/Appearance/Appearance.js
+++ app/components/screens/Appearance/Appearance.js
@@ -109,7 +109,7 @@ class Appearance extends Component {
                     : null
                 }
               >
-                Sunset
+                New
               </CelText>
             </View>
             <View>
