222cb92bc4e70e2fd79ec35d9dbb3379106ff6b9
Djuro Alfirevic <djalfirevic@gmail.com>
2020-09-16 11:50:05 +0200

    CN-7297: Add new promo code flow

--- app/redux/campaign/campaignReducer.js
+++ app/redux/campaign/campaignReducer.js
@@ -17,7 +17,7 @@ export default function campaignReducer(state = initialState, action) {
         registeredLink: action.link,
         transferHash: action.link.hash,
       };
-    case ACTIONS.SUBMIT_PROMO_CODE_SUCCESS:
+
     case ACTIONS.GET_LINK_BY_URL_SUCCESS:
       return {
         ...state,
@@ -25,6 +25,13 @@ export default function campaignReducer(state = initialState, action) {
           ...state.registeredLink,
           ...action.branchLink,
         },
+      };
+
+    case ACTIONS.SUBMIT_PROMO_CODE_SUCCESS:
+      return {
+        ...state,
+        code: action.code,
+        registeredLink: action.branchLink,
       };
 
     case ACTIONS.CHECK_PROFILE_PROMO_CODE_SUCCESS:
