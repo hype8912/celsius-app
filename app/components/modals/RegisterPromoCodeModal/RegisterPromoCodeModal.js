6c94bcf7f2991c3ce42ff6e522456bd74fb6c3b2
Djuro Alfirevic <djalfirevic@gmail.com>
2020-09-16 13:29:24 +0200

    CN-7927: Clear state

--- app/components/modals/RegisterPromoCodeModal/RegisterPromoCodeModal.js
+++ app/components/modals/RegisterPromoCodeModal/RegisterPromoCodeModal.js
@@ -69,6 +69,13 @@ class RegisterPromoCodeModal extends Component {
   closeModal = () => {
     const { actions } = this.props;
     actions.closeModal();
+
+    actions.updateFormField("promoCode", "");
+    this.setState({
+      confirmed: true,
+      loading: false,
+      hasError: false,
+    });
   };
 
   confirm = () => {
