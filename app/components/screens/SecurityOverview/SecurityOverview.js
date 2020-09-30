ec7e7acd56d26270c910d869c2b469c78f67934a
Djuro Alfirevic <djalfirevic@gmail.com>
2020-09-30 12:32:29 +0200

    CN-7793: Fix applied (#1045)

--- app/components/screens/SecurityOverview/SecurityOverview.js
+++ app/components/screens/SecurityOverview/SecurityOverview.js
@@ -196,7 +196,9 @@ class SecurityOverview extends Component {
                 enhanceText={
                   securityOverview.score_parameters.find(
                     e => e.name === "password" && e.fixable
-                  ) && "Change Password"
+                  )
+                    ? "Change Password"
+                    : null
                 }
                 onPressEnhance={() => {
                   actions.navigateTo(SCREENS.CHANGE_PASSWORD);
@@ -216,7 +218,9 @@ class SecurityOverview extends Component {
                 enhanceText={
                   securityOverview.score_parameters.find(
                     e => e.name === "pin" && e.fixable
-                  ) && "Change PIN"
+                  )
+                    ? "Change PIN"
+                    : null
                 }
                 onPressEnhance={() => {
                   actions.navigateTo(SCREENS.VERIFY_PROFILE, {
