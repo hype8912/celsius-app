deac145b9805a572606bb351fb813873de0ed9c0
Nikola Sabelnik <sabelnikola@gmail.com>
2020-10-19 15:59:52 +0200

    CN-8269 Wallet Landing Bug

--- app/components/modals/InterestDueModal/InterestDueModal.js
+++ app/components/modals/InterestDueModal/InterestDueModal.js
@@ -77,7 +77,7 @@ class InterestDueModal extends Component {
             loanAlert.id === loan.id &&
             loanAlert.type === LOAN_ALERTS.INTEREST_ALERT
         )
-      ).filter(loan => !loan.loanPaymentSettings.automatic_interest_payment)
+      ).filter(loan => loan.loanPaymentSettings && !loan.loanPaymentSettings.automatic_interest_payment)
       .sort((a, b) => a.id - b.id);
     const multipleAlerts = loansOverview.length > 1;
     if (multipleAlerts)
