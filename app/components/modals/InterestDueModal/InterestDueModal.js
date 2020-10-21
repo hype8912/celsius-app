6edc534b6a626a4ffdf161927f9ade228150f000
Nikola Sabelnik <sabelnikola@gmail.com>
2020-10-21 15:56:46 +0200

    CN-8270 crashes

--- app/components/modals/InterestDueModal/InterestDueModal.js
+++ app/components/modals/InterestDueModal/InterestDueModal.js
@@ -77,7 +77,8 @@ class InterestDueModal extends Component {
             loanAlert.id === loan.id &&
             loanAlert.type === LOAN_ALERTS.INTEREST_ALERT
         )
-      ).sort((a, b) => a.id - b.id);
+      ).filter(loan => loan.loanPaymentSettings && !loan.loanPaymentSettings.automatic_interest_payment)
+      .sort((a, b) => a.id - b.id);
     const multipleAlerts = loansOverview.length > 1;
     if (multipleAlerts)
       totalAmount = loansOverview.reduce((a, b) => {
