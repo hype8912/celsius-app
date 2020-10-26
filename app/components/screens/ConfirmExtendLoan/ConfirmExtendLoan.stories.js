a3b72e068f66b582d78e5014ae3346a88524a73b
Nikola Sabelnik <sabelnikola@gmail.com>
2020-10-26 23:04:37 +0100

    CN-8388 Stories for Extend loan flow

--- app/components/screens/ConfirmExtendLoan/ConfirmExtendLoan.stories.js
+++ app/components/screens/ConfirmExtendLoan/ConfirmExtendLoan.stories.js
@@ -3,21 +3,30 @@ import _ from "lodash";
 
 import ConfirmExtendLoan from "./ConfirmExtendLoan";
 import ScreenStoryWrapper from "../../../../storybook/stories/ScreenStoryWrapper/ScreenStoryWrapper";
-// import mockUserStore from "../../../../celsius-app-creds/mock-data/mockUserStore";
+import mockUserStore from "../../../../celsius-app-creds/mock-data/mockUserStore";
+import mockLoansStore from "../../../../celsius-app-creds/mock-data/mockLoansStore";
+import { LOAN_TYPES } from "../../../constants/DATA";
 
 const initialState = {
   user: {
-    // profile: mockUserStore.profile.postman13,
-    // appSettings: mockUserStore.appSettings.postman13,
+    profile: mockUserStore.profile.postman13,
+    appSettings: mockUserStore.appSettings.postman13,
+    bankAccountInfo: mockUserStore.bankAccountInfo.postman13
+  },
+  loans: {
+    allLoans: [],
+    loan: {},
   },
   forms: {
     formData: {},
-    }
+  },
 };
 
-const state = _.cloneDeep(initialState);
 
-const regular = () => {
+const stableCoin = () => {
+  const state = _.cloneDeep(initialState);
+  state.loans.loan = mockLoansStore.loan.stableCoin;
+  state.forms.formData.loanType = LOAN_TYPES.STABLE_COIN_LOAN;
 
   return (
     <ScreenStoryWrapper
@@ -28,6 +37,21 @@ const regular = () => {
   );
 };
 
+const dollar = () => {
+  const state = _.cloneDeep(initialState);
+  state.loans.loan = mockLoansStore.loan.USDnonUSBank;
+  state.forms.formData.loanType = LOAN_TYPES.USD_LOAN;
+
+  return (
+    <ScreenStoryWrapper
+      screenName="ConfirmExtendLoan"
+      screen={ConfirmExtendLoan}
+      state={state}
+    />
+  );
+};
+
 export default {
-  regular,
+  stableCoin,
+  dollar,
 };
