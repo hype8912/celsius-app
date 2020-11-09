5c79a38bc16f45b190606269e09abc02f2b9ccb8
Djuro Alfirevic <djalfirevic@gmail.com>
2020-11-09 15:06:49 +0100

    CN-8638: TaxReportMarketingModal added

---
+++ app/components/modals/TaxReportMarketingModal/TaxReportMarketingModal.js
@@ -0,0 +1,105 @@
+import React, { Component } from "react";
+import { Image, TouchableOpacity, View } from "react-native";
+import { bindActionCreators } from "redux";
+import { connect } from "react-redux";
+import Modal from "react-native-modal";
+
+import { MODALS, THEMES } from "../../../constants/UI";
+import CelText from "../../atoms/CelText/CelText";
+import { getColor } from "../../../utils/styles-util";
+import { COLOR_KEYS } from "../../../constants/COLORS";
+import * as appActions from "../../../redux/actions";
+import { isIos } from "../../../utils/ui-util";
+import Icon from "../../atoms/Icon/Icon";
+import CelButton from "../../atoms/CelButton/CelButton";
+import TaxReportMarketingModalStyle from "./TaxReportMarketingModal.styles";
+
+@connect(
+  state => ({
+    openedModal: state.ui.openedModal,
+    callsInProgress: state.api.callsInProgress,
+  }),
+  dispatch => ({ actions: bindActionCreators(appActions, dispatch) })
+)
+class TaxReportMarketingModal extends Component {
+  renderTop = () => {
+    const { actions } = this.props;
+    const style = TaxReportMarketingModalStyle();
+    return (
+      <View style={style.topSection}>
+        <TouchableOpacity
+          style={style.closeButton}
+          onPress={() => actions.closeModal()}
+        >
+          <Icon
+            name={"Close"}
+            height={20}
+            width={20}
+            fill={getColor(COLOR_KEYS.LINK, THEMES.UNICORN)}
+          />
+        </TouchableOpacity>
+      </View>
+    );
+  };
+
+  renderContent = () => {
+    const style = TaxReportMarketingModalStyle();
+
+    return (
+      <View style={style.wrapper}>
+        <Image
+          source={require("../../../../assets/images/tax-report-unicorn.png")}
+          style={style.image}
+        />
+        <CelText
+          margin="0 40 0 40"
+          weight="bold"
+          align="center"
+          type="H2"
+          style={style.title}
+        >
+          {"Your tax report is ready"}
+        </CelText>
+        <CelText weight="light" align="center" style={style.subtitle}>
+          {
+            "You are now able to download the tax report for the year 2019. Celsius provides you 1099s if you earned $600 or more in a given tax year."
+          }
+        </CelText>
+        <CelButton
+          style={style.button}
+          onPress={() => console.log("Download Report")}
+        >
+          {"Download Report"}
+        </CelButton>
+        <CelText
+          color={getColor(COLOR_KEYS.PRIMARY_BUTTON)}
+          onPress={() => {
+            console.log("Remind me later");
+          }}
+        >
+          Remind me later
+        </CelText>
+      </View>
+    );
+  };
+
+  render() {
+    const style = TaxReportMarketingModalStyle();
+    const { openedModal } = this.props;
+
+    return (
+      <Modal
+        style={style.container}
+        isVisible={openedModal === MODALS.TAX_REPORT_MARKETING_MODAL}
+        useNativeDriver={!isIos()}
+      >
+        <View style={style.contentWrapper}>
+          {this.renderTop()}
+          {this.renderContent()}
+        </View>
+      </Modal>
+    );
+  }
+}
+
+export default TaxReportMarketingModal;
---
+++ app/components/modals/TaxReportMarketingModal/TaxReportMarketingModal.styles.js
@@ -0,0 +1,65 @@
+import {
+  getColor,
+  getThemedStyle,
+  heightPercentageToDP,
+  widthPercentageToDP,
+} from "../../../utils/styles-util";
+import { COLOR_KEYS } from "../../../constants/COLORS";
+import { THEMES } from "../../../constants/UI";
+
+const base = {
+  container: {
+    flex: 1,
+    marginTop: heightPercentageToDP("5%"),
+    width: "100%",
+    marginLeft: 0,
+    marginBottom: 0,
+    height: heightPercentageToDP("90%"),
+    borderTopLeftRadius: 16,
+    borderTopRightRadius: 16,
+    justifyContent: "flex-start",
+    backgroundColor: getColor(COLOR_KEYS.BACKGROUND, THEMES.UNICORN),
+  },
+  topSection: {
+    height: 40,
+    marginTop: 20,
+    alignItems: "flex-end",
+  },
+  closeButton: {
+    height: 30,
+    width: 30,
+    justifyContent: "center",
+    alignItems: "center",
+    marginRight: 10,
+  },
+  wrapper: {
+    alignItems: "center",
+  },
+  image: {
+    resizeMode: "contain",
+    width: widthPercentageToDP("15%"),
+    height: heightPercentageToDP("15%"),
+    marginTop: heightPercentageToDP("10%"),
+  },
+  subtitle: {
+    marginTop: heightPercentageToDP("3%"),
+    marginRight: 35,
+    marginLeft: 35,
+  },
+  button: {
+    marginTop: heightPercentageToDP("5%"),
+    marginBottom: heightPercentageToDP("2%"),
+  },
+};
+
+const themed = {
+  light: {},
+
+  dark: {},
+
+  unicorn: {},
+};
+
+const TaxReportMarketingModalStyle = () => getThemedStyle(base, themed);
+
+export default TaxReportMarketingModalStyle;
