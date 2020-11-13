62210719db34f659fdc8066a18d43c237be7030a
fj <filip.jovakaric@gmail.com>
2020-11-13 01:07:10 +0100

    New dns great fix

--- app/utils/api-util.js
+++ app/utils/api-util.js
@@ -272,13 +272,11 @@ async function responseInterceptor(res) {
  * Intercepts every error response from server
  */
 async function errorInterceptor(serverError) {
-  console.log({ raw: serverError.raw_error })
 
   if (!serverError.raw_error) {
-    console.log("hello")
-    store.dispatch(actions.toggleMaintenanceMode(
+    return store.dispatch(actions.toggleMaintenanceMode(
       "Sorry for the inconvenience",
-      "              Celsius is currently updating its DNS servers. During this time, our systems will be temporarily unavailable. This process is expected to last up to 24 hours. We are still working really hard to fix all the problems."
+      "Celsius is currently updating its DNS servers. During this time, our systems will be temporarily unavailable. This process is expected to last up to 24 hours. We are still working really hard to fix all the problems."
     ));
   }
 
